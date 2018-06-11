ae42d0bd2326ce2888cafe160e4a6d158449e8ac
Vincent Rémond <vincent.remond@gmail.com>
2018-06-11 12:20:28 +0200

    Cleanup default pages

--- TaskManager/TaskManager.Web/Controllers/HomeController.cs
+++ TaskManager/TaskManager.Web/Controllers/HomeController.cs
@@ -10,26 +10,7 @@ namespace TaskManager.Web.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
