3c649d41d9315e2d974e5b4ae20b38a104966a40
Vincent Rémond <vincent.remond@gmail.com>
2018-06-17 20:55:38 +0200

    Semaphore & stuff

---
+++ TaskManager/TaskManager.Data.Json/JsonFileRepositoryResult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using TaskManager.Models;
+
+namespace TaskManager.Data.Json
+{
+    internal class JsonFileRepositoryResult<T> : IDisposable
+    {
+        private JsonFileRepository<T> _jsonFileRepository;
+        private readonly bool _updateOnDispose;
+        private readonly SemaphoreSlim _semaphore;
+
+        public JsonFileRepositoryResult(JsonFileRepository<T> jsonFileRepository, bool updateOnDispose, SemaphoreSlim semaphore)
+        {
+            _jsonFileRepository = jsonFileRepository;
+            _updateOnDispose = updateOnDispose;
+            _semaphore = semaphore;
+        }
+
+        public List<T> Data { get; internal set; }
+
+        public void Dispose()
+        {
+            if(_updateOnDispose)
+            {
+                _jsonFileRepository.UpdateDada(Data);
+            }
+            _semaphore.Release();
+        }
+
+        internal void Set(Predicate<T> predicate, T todo)
+        {
+            var index = Data.FindIndex(predicate);
+            if (index == -1)
+            {
+                Data.Add(todo);
+            }
+            else
+            {
+                Data[index] = todo;
+            }
+        }
+    }
+}
--- TaskManager/TaskManager.Data.Json/JsonTodoRepository.cs
+++ TaskManager/TaskManager.Data.Json/JsonTodoRepository.cs
@@ -10,63 +10,35 @@ namespace TaskManager.Data.Json
 {
     public class JsonTodoRepository : ITodoRepository
     {
-        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly JsonFileRepository<Todo> _fileRepository;
 
         public JsonTodoRepository(IHostingEnvironment hostingEnvironment)
         {
-            _hostingEnvironment = hostingEnvironment;
+            _fileRepository = new JsonFileRepository<Todo>(hostingEnvironment, "todo");
         }
 
         public Todo Get(string todoId)
         {
-            var data = ReadModel();
-            return data.SingleOrDefault(t => t.TodoId == todoId);
+            using (var todos = _fileRepository.GetDataAsReadOnly())
+            {
+                return todos.Data.SingleOrDefault(t => t.TodoId == todoId);
+            }
         }
 
         public IEnumerable<Todo> GetAllActives()
         {
-            var data = ReadModel();
-            return data.Where(t => t.Status == TodoStatus.Active);
+            using (var todos = _fileRepository.GetDataAsReadOnly())
+            {
+                return todos.Data.Where(t => t.Status == TodoStatus.Active).ToList();
+            }
         }
 
         public void Upsert(Todo todo)
         {
-            var data = ReadModel();
-            var index = data.FindIndex(t => t.TodoId == todo.TodoId);
-            if (index == -1)
+            using (var todos = _fileRepository.GetDataAsReadWrite())
             {
-                data.Add(todo);
-            } else
-            {
-                data[index] = todo;
+                todos.Set(t => t.TodoId == todo.TodoId, todo);
             }
-            WriteModel(data);
         }
-
-        #region File actions
-
-        private List<Todo> ReadModel()
-        {
-            var file = GetFilePath();
-            if (!File.Exists(file))
-            {
-                return new List<Todo>();
-            }
-            var contents = File.ReadAllText(file);
-            return JsonConvert.DeserializeObject<List<Todo>>(contents);
-        }
-
-        private void WriteModel(List<Todo> data)
-        {
-            var file = GetFilePath();
-            var contents = JsonConvert.SerializeObject(data, Formatting.Indented);
-            File.WriteAllText(file, contents);
-        }
-
-        private string GetFilePath()
-        {
-            return Path.Combine(_hostingEnvironment.ContentRootPath, "Data", "data.json");
-        }
-        #endregion
     }
 }
