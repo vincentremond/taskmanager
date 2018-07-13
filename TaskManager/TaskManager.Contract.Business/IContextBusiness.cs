1547684e23096d4dea89c47ed2717ec374d69c1d
Vincent Rémond <vincent.remond@gmail.com>
2018-07-13 17:40:59 +0200

    Add repo & edit page

---
+++ TaskManager/TaskManager.Contract.Business/IContextBusiness.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TaskManager.Models;
+
+namespace TaskManager.Contract.Business
+{
+    public interface IContextBusiness
+    {
+        IEnumerable<Context> GetAll();
+        Context Get(string contextId);
+        void SaveChanges(Context todo);
+        void Create(string title);
+        void Delete(string contextId);
+    }
+}
--- TaskManager/TaskManager.Data.Json/JsonTodoRepository.cs
+++ TaskManager/TaskManager.Data.Json/JsonTodoRepository.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.Hosting;
-using Newtonsoft.Json;
 using TaskManager.Contract.Data;
 using TaskManager.Models;
 
