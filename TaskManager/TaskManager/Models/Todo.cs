5467fe81aa07d0d327fca0a3494ba2f3ee9f67e0
Vincent Rémond <vincent.remond@gmail.com>
2018-07-16 13:49:01 +0200

    Review data structure

--- TaskManager/TaskManager/Models/Todo.cs
+++ TaskManager/TaskManager/Models/Todo.cs
@@ -5,7 +5,7 @@ namespace TaskManager.Models
     public class Todo
     {
         public string TodoId { get; set; }
-        public TodoStatus Status { get; set; }
+        public bool Completed { get; set; }
         public int Score { get; set; }
         public string Title { get; set; }
         public int Complexity { get; set; }
