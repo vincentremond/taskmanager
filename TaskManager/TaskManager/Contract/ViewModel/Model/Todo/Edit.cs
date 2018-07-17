5cf9eda0230e9d3695ba754ad24b1701962bd8c8
Vincent Rémond <vincent.remond@gmail.com>
2018-07-17 16:22:27 +0200

    Url

--- TaskManager/TaskManager/Contract/ViewModel/Model/Todo/Edit.cs
+++ TaskManager/TaskManager/Contract/ViewModel/Model/Todo/Edit.cs
@@ -12,5 +12,6 @@ namespace TaskManager.Contract.ViewModel.Model.Todo
         public string Description { get; set; }
         public string ContextId { get; set; }
         public string ProjectId { get; set; }
+        public string Url { get; set; }
     }
 }
