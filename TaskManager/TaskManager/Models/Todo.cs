0ca3d888dce65bfc4a8982830be960d432685c60
Vincent Rémond <vincent.remond@gmail.com>
2018-07-17 16:22:27 +0200

    Url

--- TaskManager/TaskManager/Models/Todo.cs
+++ TaskManager/TaskManager/Models/Todo.cs
@@ -14,6 +14,7 @@ namespace TaskManager.Models
         public string Description { get; set; }
         public Context Context { get; set; }
         public Project Project { get; set; }
+        public string Url { get; set; }
     }
 
 }
